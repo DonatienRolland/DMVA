78b9a61df1d6eb70400d80a3a00218803df72492
Donatien Rolland <donatien@rollandmail.com>
2018-01-30 18:37:55 +0100

    new banner

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,7 +2,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
  include Cloudinary::CarrierWave
 
-   process eager: true  # Force version generation at upload time.
+  process eager: true  # Force version generation at upload time.
 
   process convert: 'jpg'
 
