e008aa5d967552d0145ef6b192d1a507cfb8d3c0
Donatien Rolland <donatien@rollandmail.com>
2018-02-08 19:02:09 +0100

    solve

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
+  get 'about', to: 'pages#apropos', as: :apropos
+
   # get 'contact', to: 'pages#contact',  as: :contact
   get 'realisations', to: 'pages#realisation',  as: :realisations
 
@@ -9,8 +10,7 @@ Rails.application.routes.draw do
 
 match '/contact',     to: 'contacts#index',             via: 'get'
 resources "contacts", only: [:index, :create]
-=======
-  get 'about', to: 'pages#apropos', as: :apropos
->>>>>>> 0d76548673c3f0cf1e18d2baee7216906d58341c
+
+
 
 end
