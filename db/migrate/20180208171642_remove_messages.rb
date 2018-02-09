72b01f6ac10cbd318b01815a28bfbcfca2a1135b
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2018-02-09 16:11:30 +0100

    test dorian

--- db/migrate/20180208171642_remove_messages.rb
+++
@@ -1,4 +0,0 @@
-class RemoveMessages < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208172428) do
+ActiveRecord::Schema.define(version: 20180208171642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,6 @@ ActiveRecord::Schema.define(version: 20180208172428) do
     t.string "titre"
     t.text "description"
     t.string "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
