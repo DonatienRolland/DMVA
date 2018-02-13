6c1fcb57ee718bf10b7cef6a06aca6d08ddf9fee
Donatien Rolland <donatien@rollandmail.com>
2018-02-13 15:42:09 +0100

    create a message table to  solve problem with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208172428) do
+ActiveRecord::Schema.define(version: 20180213142157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20180208172428) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
