a5a8e57b344b36e2556737ebee3597257b5426a8
Donatien Rolland <donatien@rollandmail.com>
2018-02-14 11:18:56 +0100

    test d un autre systeme de mail

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,17 +1,26 @@
-class Contact
+class Contact < MailForm::Base
+  attribute :name, :validate => true
+  attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :phone, :validate => true
+  attribute :objet, :validate => true
+  attribute :message, :validate => true
+  attribute :nickname, :captcha  => true
 
-  include ActiveModel::Model
-  include ActiveModel::Conversion
-  include ActiveModel::Validations
 
-  attr_accessor :name, :email, :objet, :message, :phone
 
-  validates :name, presence: true
-  validates :email, presence: true
-  # ,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  validates :phone, presence: true
-  validates :objet, presence: true
-  validates :message, presence: true
+
+  # include ActiveModel::Model
+  # include ActiveModel::Conversion
+  # include ActiveModel::Validations
+
+  # attr_accessor :name, :email, :objet, :message, :phone
+
+  # validates :name, presence: true
+  # validates :email, presence: true
+  # # ,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  # validates :phone, presence: true
+  # validates :objet, presence: true
+  # validates :message, presence: true
   # validates :file,      :attachment => true
   # validates :nickname,  :captcha  => true
 
