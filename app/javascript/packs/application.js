1a1d986a03e5d3e3f4ef5863f0213798c622b400
Donatien Rolland <donatien@rollandmail.com>
2018-02-20 12:49:48 +0100

    partenaire done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import "bootstrap";
 import "../components/readMore";
 import "../components/map";
 import "../components/counterOnScroll";
+import "../components/carouselResponsive";
 import { organizeCarousel } from "../components/carousel";
 
 
