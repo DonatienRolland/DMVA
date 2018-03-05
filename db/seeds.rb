deebf2ade12a873ae6222ebf54d539492d950b78
Donatien Rolland <donatien@rollandmail.com>
2018-03-05 10:33:31 +0100

    modifiaction erwan

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ puts "Donatien created"
 
 puts "Creating Articles"
 article1 = Article.new(titre: "Traitement des eaux", description: "Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum.")
-url1 = "http://res.cloudinary.com/djw9hvp9l/image/upload/v1516974822/site-1707146_1920_dabz41.jpg"
+url1 = "http://res.cloudinary.com/djw9hvp9l/image/upload/v1520188246/videomarcheur_plgygf.jpg"
 article1.remote_photo_url = url1
 puts "in process"
 article1.user = donatien
@@ -16,21 +16,21 @@ article1.save!
 puts "article1 created"
 
 article2 = Article.new(titre: "Maçonnerie", description: "Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum.")
-url2 = "http://res.cloudinary.com/djw9hvp9l/image/upload/v1516974819/bulldozer-679989_1920_l9oyzt.jpg"
+url2 = "http://res.cloudinary.com/djw9hvp9l/image/upload/v1516974819/traitement_dtvkli.jpg"
 article2.remote_photo_url = url2
 article2.user = donatien
 article2.save!
 puts "article2 created"
 
 article3 = Article.new(titre: "Revetemment des surfaces publics", description: "Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum.")
-url3 = "http://res.cloudinary.com/djw9hvp9l/image/upload/v1516974820/dock-1673688_1920_nqly9v.jpg"
+url3 = "http://res.cloudinary.com/djw9hvp9l/image/upload/v1516974820/demolition_cwqyyg.jpg"
 article3.remote_photo_url = url3
 article3.user = donatien
 article3.save!
 puts "article3 created"
 
 article4 = Article.new(titre: "Demolissement d'immeuble", description: "Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum. Lorem ipsum dolor sit amet, nam aliquip quaeque ne. Sint scribentur interpretaris id pro, molestie accusamus interesset sed ne, vidisse scripta ea eum.")
-url4 = "http://res.cloudinary.com/djw9hvp9l/image/upload/v1516974822/site-1707146_1920_dabz41.jpg"
+url4 = "http://res.cloudinary.com/djw9hvp9l/image/upload/v1516974822/construction-machine-976200_1920_civams.jpg"
 article4.remote_photo_url = url4
 article4.user = donatien
 article4.save!
